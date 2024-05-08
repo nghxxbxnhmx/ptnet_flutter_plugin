6365b7af995a3bcf128cd5ade4233a1c28d484e7
Zion Nguyễn <94814450+hungiter@users.noreply.github.com>
2024-05-08 16:50:23 +0700

    MethodCallHandle - Kotlin side - process->return

---
+++ android/src/main/kotlin/com/example/ptnet_plugin/LibraryHandler.kt
@@ -0,0 +1,39 @@
+package com.example.ptnet_plugin
+
+import com.ftel.ptnetlibrary.services.*
+import com.ftel.ptnetlibrary.dto.*
+import com.google.gson.Gson
+import java.util.concurrent.*
+class LibrariesHandler(
+    act: String,
+    address: String = "",
+    server: String = "",
+    ttl: Int = -1,
+    port: Int = -1
+) {
+    private lateinit var result: String
+
+    init {
+        when (act) {
+            "ping" -> result = pingResult(address)
+            "pageLoad" -> result = pageLoadResult(address).get()
+        }
+    }
+
+    private fun pingResult(address: String): String {
+        val pingdto: PingDTO = PingService().execute(address = address)
+        return Gson().toJson(pingdto)
+    }
+
+    private fun pageLoadResult(address: String): CompletableFuture<String> {
+        return CompletableFuture.supplyAsync {
+            val pageLoadService = PageLoadService()
+            try {
+                val time = pageLoadService.pageLoadTimer(address)
+                Gson().toJson(mapOf("address" to address, "time" to time))
+            } catch (e: Exception) {
+                throw RuntimeException("Failed to load page: ${e.message}")
+            }
+        }
+    }
+}
--- android/src/main/kotlin/com/example/ptnet_plugin/PtnetPlugin.kt
+++ android/src/main/kotlin/com/example/ptnet_plugin/PtnetPlugin.kt
@@ -21,15 +21,32 @@ class PtnetPlugin: FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(this)
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
-  }
-
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    // Get data from dart
+    val map: Map<String, Any?> = call.arguments as Map<String, Any?>
+
+    val tmpAdd = (if (map.containsKey("address")) map["address"] else "8.8.8.8") as String
+    val port = (if (map.containsKey("port")) map["port"] else -1) as Int
+    // Note: Lỗi ở Ping, reset máy thử ( recommend: ko dùng AVD-Android Virtual Device (AndroidStudio-setting cực)
+    when (call.method) {
+      "getPingResult" -> result.success(
+        LibrariesHandler(
+          "ping",
+          address = tmpAdd
+        ).getResult()
+      )
+
+      "getPageLoadResult" -> result.success(
+        LibrariesHandler(
+          "pageLoad",
+          address = tmpAdd
+        ).getResult()
+      )
+
+      else -> result.notImplemented()
+    }
   }
 }
